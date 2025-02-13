aead04ebef043e516b71234050e0e1a709cbb3d2
harunfrzq <harunfathurrozaq01@gmail.com>
2025-02-13 22:41:42 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // ini file javascript
+
+console.log('javascript is ready');
+
+function validateform() {
+    let input = document.getElementById('main-input');
+    console.log(input.value);
+
+    if (input.value =='') {
+        alert('Tolong diisi inputan');
+    }else{
+        ////proses konversi
+
+    let calc = convertTocelcius(input.value);
+    ///tampilkan hasil
+    document.getElementById('main-result').value = calc;
+    document.getElementById('cara-konversi').value = 'Diketahui : C=' + input.value + 'F..';
+    console.log('berhasil di eksekusi ')};
+}
+   let convertTocelcius = (input)=>{
+    return input*9/5 +32;
+   }
